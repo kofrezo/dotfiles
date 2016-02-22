a82756e77da1110f3ea0a348c38d83174c5ea568
Daniel Kröger <daniel.kroeger@innogames.com>
2016-02-22 15:19:48 +0100

    [FEATURE] added install script and vim airline setup

--- .vimrc
+++ .vimrc
@@ -1,4 +1,23 @@
-# enable vim pathogen plugin loader
+" Vim Editor Setup
+"
+" author : daniel.kroeger@kofrezo.io
+" version: 22.02.2016
+
+" Enable pathogen plugin autoloader
+runtime bundle/vim-pathogen/autoload/pathogen.vim
 execute pathogen#infect()
-syntax on
+
+" Enable filetype plugins
 filetype plugin indent on
+
+" Enable and setup vim-airline plugin
+set laststatus=2
+let g:airline_theme = 'luna'
+let g:airline_powerline_fonts = 1
+
+" Setup basic appereance
+syntax on
+set expandtab
+set enc=utf-8
+set tabstop=4
+set colorcolumn=80
--- install.sh
+++ install.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 
+## UPDATE SUBMODULES ##########################################################
+
+echo "* updating submodules"
+git submodule update
+
 ## SETUP VIM ##################################################################
 
-if [ -d ~/.vim ]; then
-  echo "creating backup for old vim config to ~/.vim.bak"
-  mv ~/.vim ~/.vim.bak
-  ln -s $(pwd)/.vim ~/.vim
+if [ -d ~/.vim ] || [ ! -x ~/.vim ]; then
+    if [ -d ~/.vim ]; then
+        echo "* creating backup of existing .vim folder"
+        mv ~/.vim ~/.vim.bak
+    fi
+    echo "* installing new .vim folder"
+    ln -s $(pwd)/.vim ~/.vim
 fi
 
-if [ -f ~/.vimrc ]; then
-  echo "creating backup for old vim config to ~/.vimrc.bak"
-  mv ~/.vimrc ~/.vimrc.bak
-  ln -s $(pwd)/.vimrc ~/.vimrc
+if [ -f ~/.vimrc ] || [ ! -x ~/.vimrc ]; then
+    if [ -f ~/.vimrc ]; then
+        echo "* creating backup of exiting .vimrc"
+        mv ~/.vimrc ~/.vimrc.bak
+    fi
+    echo "* installing new .vimrc"
+    ln -s $(pwd)/.vimrc ~/.vimrc
 fi
 
-###############################################################################
+echo "* Installing powerline fonts for vim airline plugin"
+sh $(pwd)/fonts/install.sh
+
+## SETUP BASH #################################################################
+
+if [ -f ~/.bashrc ]; then
+    echo "* creating backup of existing .bashrc"
+    mv ~/.bashrc ~/.bashrc.bak
+    echo "* installing new .bashrc"
+    ln -s $(pwd)/.bashrc ~/.bashrc
+fi
