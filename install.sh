91081d989080173ec950b4db417881a64930f6c6
Daniel Kröger <daniel.kroeger@innogames.com>
2017-01-03 22:27:53 +0100

    [BUGFIX] Add missing pip requirments and cleanup install script

--- install.sh
+++ install.sh
@@ -2,35 +2,54 @@
 
 ## UPDATE SUBMODULES ##########################################################
 
-echo "* updating submodules"
+echo '* updating submodules'
 git submodule init
 git submodule update --recursive
 
+## INSTALL REQUIRMENTS ########################################################
+
+echo '* installing pip dependencies'
+pip install -r requirements.txt &> /dev/null
+
 ## SETUP VIM ##################################################################
 
-if [ -d ~/.vim ] || [ ! -x ~/.vim ]; then
-    if [ -d ~/.vim ]; then
-        echo "* creating backup of existing .vim folder"
+if [[ -d ~/.vim ]]; then
+    if [[ -L ~/.vim ]]; then
+        echo '* deleting old symlink'
+        unlink ~/.vim
+    else
+        echo '* creating backup of existing .vim folder'
         mv ~/.vim ~/.vim.bak
     fi
-    echo "* installing new .vim folder"
+
+    echo '* linking new .vim folder'
     ln -s $(pwd)/.vim ~/.vim
 fi
 
-if [ -f ~/.vimrc ] || [ ! -x ~/.vimrc ]; then
-    if [ -f ~/.vimrc ]; then
-        echo "* creating backup of exiting .vimrc"
+if [[ -f ~/.vimrc  ]]; then
+    if [[ -L ~/.vimrc ]]; then
+        echo '* deleting old symlink'
+        unlink ~/.vimrc
+    else
+        echo '* creating backup of exiting .vimrc'
         mv ~/.vimrc ~/.vimrc.bak
     fi
-    echo "* installing new .vimrc"
+
+    echo '* linking new .vimrc'
     ln -s $(pwd)/.vimrc ~/.vimrc
 fi
 
 ## SETUP BASH #################################################################
 
-if [ -f ~/.bashrc ]; then
-    echo "* creating backup of existing .bashrc"
-    mv ~/.bashrc ~/.bashrc.bak
-    echo "* installing new .bashrc"
+if [[ -f ~/.bashrc ]]; then
+    if [[ -L ~/.bashrc ]]; then
+        echo '* deleting old symlink'
+        unlink ~/.bashrc
+    else
+        echo '* creating backup of existing .bashrc'
+        mv ~/.bashrc ~/.bashrc.bak
+    fi
+
+    echo '* linking .bashrc'
     ln -s $(pwd)/.bashrc ~/.bashrc
 fi
