503650083df6e56889c8f9fb32135110db508860
kofrezo <daniel.kroeger@kofrezo.io>
2020-10-27 22:08:58 +0100

    Ensure apps log to $HOME/.logs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,4 +3,4 @@
 # Terminate already running bar instances
 killall -q polybar
 
-polybar -c $HOME/.config/polybar/config.ini top >> $HOME/polybar.log 2>&1 & disown
+polybar -c $HOME/.config/polybar/config.ini top >> $HOME/.logs/polybar.log 2>&1 & disown
